7ef72c36ecf2d6556f0bfbec0202de84982576e3
Medetcan Diler <medetdiler1996@gmail.com>
2023-05-06 23:14:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards( arr ){
+  const msg = [];
+  for(let i of arr){
+    msg.push(`Thank you, ${i}, for the wonderful surprise gift!`)
+  }
+  return msg
+}
+
+function countDown(){
+  for( let count = 10; count >= 0; count--){
+    console.log(count)
+  }
+}
